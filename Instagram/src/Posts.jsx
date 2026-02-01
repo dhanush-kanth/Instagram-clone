c5bb32e560deb1198a756a42cbfd095503126be5
dhanush-kanth <dhanushkanth661@gmail.com>
2026-02-01 15:29:31 +0530

    Working on the Feeds Design

--- Instagram/src/Posts.jsx
+++ Instagram/src/Posts.jsx
@@ -4,20 +4,20 @@ function Posts() {
   const[post , setpost] = useState([])
 
   useEffect(()=>{
-    fetch('http://localhost:5000/posts')
+    fetch('http://localhost:3000/posts')
     .then((data)=> data.json())
     .then((data)=>(setpost(data)))
     .catch((error)=> console.log (error))
   },[])
   return (
-    <div>
+    <div className='d-flex justify-content-center'>
       {post.length > 0 ? 
-        <div className='mt-4 '>
+        <div>
           {post.map((post)=> (
-            <div key={post.id}>
+            <div className="my-3" key={post.id}>
               <div className='flex d-flex pt-2'>
               <img className="profile-pic rounded-circle" src={post.profilePic} alt="profile" />
-              <h5>{post.username}</h5>
+              <h5>{post.username}</h5>  
               </div>
               <img className="post-image " src={post.postImage} alt="post" />
               <div className='flex d-flex'>
@@ -25,6 +25,10 @@ function Posts() {
                 <h4><i class="bi bi-chat"></i></h4>
                 <h4><i class="bi bi-send"></i></h4>
               </div>
+              <div className='flex d-flex flex-column'>
+              <h6>{post.likes} likes</h6>
+              <p>{post.caption}</p>
+            </div>
             </div>
           ))}
         </div>
--- Instagram/src/Sidebar.jsx
+++ Instagram/src/Sidebar.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 function Sidebar() {
   return (
     <>
-    <div className='m-3'>
+    <div className='m-3 position-fixed '>
     <div className='d-flex flex-column gap-3'>
-    <img className="text-logo" src='src/assets/Instagram_text.png'/>
+    <img className="text-logo mt-2" src='src/assets/Instagram_text.png'/>
+    <div className='d-flex flex-column gap-4 mt-4 fs-5 text'>
     <div><i className="bi bi-house-door"></i>Home</div>
     <div><i className="bi bi-search"></i>Search</div>
     <div><i className="bi bi-compass"></i>Explore</div>
@@ -14,6 +15,7 @@ function Sidebar() {
     <div><i className="bi bi-heart"></i>Notification</div>
     <div><i className="bi bi-plus-square"></i>Create</div>
     <div><i className="bi bi-person-circle"></i>Profile</div>
+    </div>
     </div>
     <div className='d-flex flex-column gap-3 fixed-bottom mb-4 m-3'>
         <div><i className="bi bi-threads"></i>Threads</div>
