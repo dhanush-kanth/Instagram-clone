ec581e2f5a9417237d99a593a559d9ad96f20bd2
dhanush-kanth <dhanushkanth661@gmail.com>
2026-02-16 13:59:30 +0530

    Working on the Suggestion Design

--- Instagram/src/Suggestion.jsx
+++ Instagram/src/Suggestion.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 
 function Suggestion() {
+
+  const [profile, setprofile] = useState(null)
+  const [suggestion, setsuggestion] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/profile')
+    .then((data => data.json()))
+    .then((data) => setprofile(data))
+    .catch((error) => console.log(error))
+
+    fetch('http://localhost:3000/suggestion')
+    .then((data => data.json())
+    .then((data) => setsuggestion(data))
+    .catch((error) => console.log(error))
+  )
+  },[]);
   return (
-    <div>Suggestions</div>
+    <div>
+      <div className='flex d-flex pt-2'>
+      <img className="profile-pic rounded-circle" src={profile.profilePic} alt="profile" />
+      <h5>{profile.username}</h5>  
+      </div>
+    </div>
   )
 }
 
