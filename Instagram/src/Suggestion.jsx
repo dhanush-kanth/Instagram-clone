6211e34e02896914d805aaba585aae48de23ad35
dhanush-kanth <dhanushkanth661@gmail.com>
2026-03-04 12:40:53 +0530

    Working on the Suggestion Design

--- Instagram/src/Suggestion.jsx
+++ Instagram/src/Suggestion.jsx
@@ -1,31 +1,67 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
 
 function Suggestion() {
-
-  const [profile, setprofile] = useState(null)
-  const [suggestion, setsuggestion] = useState(null)
+  const [profile, setProfile] = useState(null);
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/profile')
-    .then((data => data.json()))
-    .then((data) => setprofile(data))
-    .catch((error) => console.log(error))
+    fetch("http://localhost:3000/profile")
+      .then((res) => res.json())
+      .then((data) => setProfile(data))
+      .catch((error) => console.log(error));
 
-    fetch('http://localhost:3000/suggestion')
-    .then((data => data.json())
-    .then((data) => setsuggestion(data))
-    .catch((error) => console.log(error))
-  )
-  },[]);
+    fetch("http://localhost:3000/Suggestions")
+      .then((res) => res.json())
+      .then((data) => setSuggestions(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div>
-      <div className='flex d-flex pt-2'>
-      <img className="profile-pic rounded-circle" src={profile.profilePic} alt="profile" />
-      <h5>{profile.username}</h5>  
+      <div className="suggestions w-75 m-4">
+        {profile ? (
+          <div className="flex d-flex pt-2">
+            <img
+              className="profile-pic rounded-circle"
+              src={profile.profilePic}
+              alt="profile"
+            />
+            <h5>{profile.username}</h5>
+            <p className="ms-auto text-primary">Switch</p>
+          </div>
+        ) : (
+          <p>Loading Page</p>
+        )}
+
+        <div className="d-flex ">
+          <p>Suggestions for you</p>
+          <b className="ms-auto">see more</b>
+        </div>
+
+        {suggestions && suggestions.length > 0 ? (
+          <div>
+            {suggestions.map((item) => (
+              <div className="my-3" key={item.id}>
+                <div className="flex d-flex pt-2">
+                  <img
+                    className="profile-pic rounded-circle"
+                    src={item.profilePic}
+                    alt="profile"
+                  />
+                  
+                  <h5>{item.username}</h5>
+                  <p className="ms-auto text-primary">Follow</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading</p>
+        )}
+        
       </div>
     </div>
-  )
+  );
 }
 
-export default Suggestion
+export default Suggestion;
