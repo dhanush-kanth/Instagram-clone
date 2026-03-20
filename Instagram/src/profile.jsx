c861d9f7eb9890ed0b87367f1fecae8e02d0614b
DHANUSH KANTH S <dhanushkanth661@gmail.com>
2026-03-20 16:59:06 +0530

    Update profile picture class name in profile.jsx

--- Instagram/src/profile.jsx
+++ Instagram/src/profile.jsx
@@ -14,7 +14,7 @@ function profile() {
     <div>
         {
             profile ? (<div className= "m-5  align-items-center profile-pic">
-                <img src={profile.profilePic} alt={profile.name} className="profile-pic rounded-circle" />
+                <img src={profile.profilePic} alt={profile.name} className="profilepage-pic rounded-circle" />
                 <h5>{profile.username}</h5>
                 <a href="https://www.instagram.com/dhanush_kanth/"> Edit Profile</a>
             </div>) : (<div><p>Loading</p></div>)
